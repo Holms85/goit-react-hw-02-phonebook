8a07236e8ecbe28506cd91382e739cc19f40be5d
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-08-09 22:06:58 +0300

    finaly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,23 +16,6 @@ class App extends Component {
     filter: '',
   };
 
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
-
-  //   if (parsedContacts) {
-  //     this.setState({ contacts: parsedContacts });
-  //   }
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   const { contacts } = this.state;
-
-  //   if (contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(contacts));
-  //   }
-  // }
-
   addContacts = data => {
     const { contacts } = this.state;
 
